afd9a756295dc80ba576d5ce73e81dc0af712989
gmhridu <hasanhridoymahabub9@gmail.com>
2025-04-23 20:18:46 +0600

    test

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -36,7 +36,7 @@ export const store = configureStore({
       },
     }).concat(baseApi.middleware),
   // 👇 This is all you need to hide DevTools in production
-  devTools: config.node_env === "development",
+  devTools: true,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
